0002d6787a8c15812ca58e4c24f54ca7b4486986
SiarheiSys <sissergey@yandex.by>
2021-09-12 08:09:31 +0300

    /

--- src/test/java/org/example/GoogleReturnToStartPageTest.java
+++ src/test/java/org/example/GoogleReturnToStartPageTest.java
@@ -3,14 +3,10 @@ package org.example;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
-import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class GoogleReturnToStartPageTest {
